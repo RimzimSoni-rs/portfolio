74b6ca38164fbe096e72f9050b89959fb812ae14
Rimzim Soni <sonirimzim3@gmail.com>
2024-12-06 01:04:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Tech Enthusiastic", "CS student", "Learner + Coder"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
